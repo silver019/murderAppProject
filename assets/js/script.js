82c844d08823699c9a81805588f9a0f5cd255d6f
Built-in Administrator <localadmin@USA-MIA-L006.marineharvest.net>
2017-12-12 20:48:39 -0500

    template

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    console.log( "ready!" );
+ 
+
+		// objects 
+			
+
+		// variables
+ 
+			
+		// arrays
+		
+		// functions
+				
+		// console.log
+
+				
+		// DOM
+
+
+
+
+});
+
