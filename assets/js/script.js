d6dad652648ba64c37c17b6cc8624801e5201342
Built-in Administrator <localadmin@USA-MIA-L006.marineharvest.net>
2017-12-14 20:14:14 -0500

    map with hover working

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,22 +3,21 @@ $( document ).ready(function() {
     console.log( "ready!" );
  
 
-		// objects 
-			
+//initialize your Kartograph map 
+var map = kartograph.map('#map')
 
-		// variables
- 
-			
-		// arrays
-		
-		// functions
-				
-		// console.log
+var map = kartograph.map('#map', 600, 400);
 
-				
-		// DOM
-
-
+$("#MO, #MS, #MT, #NC, #ND, #NE, #NH, #NJ, #NM, #NV, #NY, #OH, #OK, #OR, #PA, #RI, #SC, #SD, #TN, #TX, #UT, #VA, #VT, #WA, #WI, #WV, #WY, #AK, #HI, #AL, #AR, #AZ, #CA, #CO, #CT, #DE, #FL, #GA, #IA, #ID, #IL, #IN, #KS, #KY, #LA, #MA, #MD, #ME, #MI, #MN").hover(function () {
+    //stuff to do on mouse enter
+var col = "#FF69B4";
+    $(this).attr("fill", col );
+}, 
+function () {
+    //stuff to do on mouse leave
+    var origCol ="#D3D3D3";
+     $(this).attr("fill", origCol);
+});
 
 
 });
